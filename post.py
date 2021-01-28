1dbfed07243634e0db20eae53d5141a1e9dc2bef
Eric02851 <58347689+Eric02851@users.noreply.github.com>
2021-01-28 11:40:07 -0800

    Added email functionality

--- post.py
+++ post.py
@@ -1,4 +1,13 @@
 import requests
+from hidden import schoolSignIn
+import smtplib, ssl
+
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email = schoolSignIn.email
+receiver_email = schoolSignIn.email
+password = schoolSignIn.password
+context = ssl.create_default_context()
 
 def post(shortUrl, date):
     s = requests.Session()
@@ -15,21 +24,16 @@ def post(shortUrl, date):
     index = getResponse.find('''[[746616177,"Today's Code",null,0,[[280336572,null,1,null,[[2,100,["''')
     indexEnd = getResponse.find('''"]
 ,"Nope. Wrong answer."]''')
-
     todaysCode = getResponse[index+68:indexEnd]
-    print(todaysCode)
-    print(date.month)
-    print(date.day)
-    print(date.year)
 
     form_data = {
         "entry.280336572": todaysCode,
 
-        "entry.1529735670": "114966",
+        "entry.1529735670": schoolSignIn.studentId,
 
         "pageHistory": "",
 
-        "entry.2096777452": "Eric Still",
+        "entry.2096777452": schoolSignIn.studentName,
 
         "entry.985620564": "10.0",
 
@@ -42,5 +46,27 @@ def post(shortUrl, date):
         "pageHistory": "0,1"
     }
 
-    postResponse = s.post(responseUrl, data=form_data)
-    print(postResponse)
+    postResponse = str(s.post(responseUrl, data=form_data))
+    email(postResponse, todaysCode, date)
+
+def email(postResponse, todaysCode, date):
+    if postResponse == "<Response [200]>":
+        message = f"""\
+Subject: Assessment {date.month}/{date.day} COMPLETED
+
+Assessment Content:
+    Today's Code: {todaysCode}
+    Student ID Number: {schoolSignIn.studentId}
+    Your Full First Name: {schoolSignIn.studentName}
+    Date: {date.month}/{date.day}/{date.year}
+    Numerical Grade: 10.0
+"""
+    else:
+        message = """
+Subject: Assessment {date.month}/{date.day} FAILED
+    Something went wrong.
+"""
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        print("Email Sent")
