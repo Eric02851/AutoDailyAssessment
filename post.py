c8a5843df3f473765db8742bf2b340b55eee9e40
Eric02851 <58347689+Eric02851@users.noreply.github.com>
2021-01-31 18:12:20 -0800

    Bad url fix and url now sends with email

--- post.py
+++ post.py
@@ -9,8 +9,32 @@ receiver_email = schoolSignIn.email
 password = schoolSignIn.emailPassword
 context = ssl.create_default_context()
 
-def post(shortUrl, date):
-    s = requests.Session()
+s = requests.Session()
+
+def email(postResponse, todaysCode, shortUrl, date):
+    if postResponse == "<Response [200]>":
+        message = f"""\
+Subject: Assessment {date.month}/{date.day} COMPLETED
+
+Assessment url: {shortUrl}
+Assessment Content:
+    Today's Code: {todaysCode}
+    Student ID Number: {schoolSignIn.studentId}
+    Your Full First Name: {schoolSignIn.studentName}
+    Date: {date.month}/{date.day}/{date.year}
+    Numerical Grade: 10.0
+"""
+    else:
+        message = """
+Subject: Assessment {date.month}/{date.day} FAILED
+    Something went wrong.
+"""
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        print("Email Sent")
+
+def getCode_Url(shortUrl):
     viewUrl = s.get(shortUrl).url
     splitUrl = viewUrl.split('/')
 
@@ -26,8 +50,16 @@ def post(shortUrl, date):
 ,"Nope. Wrong answer."]''')
     todaysCode = getResponse[index+68:indexEnd]
 
+    class getData:
+        code = todaysCode
+        url = responseUrl
+    return getData
+
+def post(shortUrl, date):
+    getData = getCode_Url(shortUrl)
+
     form_data = {
-        "entry.280336572": todaysCode,
+        "entry.280336572": getData.code,
 
         "entry.1529735670": schoolSignIn.studentId,
 
@@ -46,27 +78,5 @@ def post(shortUrl, date):
         "pageHistory": "0,1"
     }
 
-    postResponse = str(s.post(responseUrl, data=form_data))
-    email(postResponse, todaysCode, date)
-
-def email(postResponse, todaysCode, date):
-    if postResponse == "<Response [200]>":
-        message = f"""\
-Subject: Assessment {date.month}/{date.day} COMPLETED
-
-Assessment Content:
-    Today's Code: {todaysCode}
-    Student ID Number: {schoolSignIn.studentId}
-    Your Full First Name: {schoolSignIn.studentName}
-    Date: {date.month}/{date.day}/{date.year}
-    Numerical Grade: 10.0
-"""
-    else:
-        message = """
-Subject: Assessment {date.month}/{date.day} FAILED
-    Something went wrong.
-"""
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-        print("Email Sent")
+    postResponse = str(s.post(getData.url, data=form_data))
+    email(postResponse, getData.code, shortUrl, date)
